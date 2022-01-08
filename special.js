e73da36ea3235ced501cd63dccc45f517b317a36
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2022-01-08 17:09:13 +0100

    make-up

--- special.js
+++ special.js
@@ -24,3 +24,29 @@ $(".special").owlCarousel({
     },
   },
 });
+
+$(".main_features").owlCarousel({
+  margin: 0,
+  autoplay: true,
+  loop: true,
+  dots: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      margin: 10,
+      dots: true,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      margin: 10,
+      nav: false,
+    },
+  },
+});
