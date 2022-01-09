3deb20b91e476b732824dfa787ea93f5d2aa22f6
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2022-01-09 22:03:50 +0100

    pills

--- main.js
+++ main.js
@@ -1,44 +1,56 @@
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+// const progress = document.getElementById("progress");
+// const prev = document.getElementById("prev");
+// const next = document.getElementById("next");
+const slideInfo = document.querySelectorAll(".big-number-circle");
+// const circles = document.querySelectorAll(".circle");
 
-let currentActive = 1;
-
-next.addEventListener("click", () => {
-  currentActive++;
-
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
-  }
-
-  update();
-});
-
-prev.addEventListener("click", () => {
-  currentActive--;
-
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
-
-  update();
-});
-
-function update() {
-  circles.forEach((circle, idx) => {
-    if (idx < currentActive) {
-      circle.classList.add("on");
+const carouselButton = (slideNumber) => {
+  slideInfo.forEach((slide, index) => {
+    if (index === slideNumber) {
+      slide.classList.add("shownum");
     } else {
-      circle.classList.remove("on");
+      slide.classList.remove("shownum");
     }
   });
+};
+console.log(carouselButton);
 
-  const actives = document.querySelectorAll(".on");
+// let currentActive = 1;
 
-  progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
-}
+// next.addEventListener("click", () => {
+//   currentActive++;
+
+//   if (currentActive > circles.length) {
+//     currentActive = circles.length;
+//   }
+
+//   update();
+// });
+
+// prev.addEventListener("click", () => {
+//   currentActive--;
+
+//   if (currentActive < 1) {
+//     currentActive = 1;
+//   }
+
+//   update();
+// });
+
+// function update() {
+//   circles.forEach((circle, idx) => {
+//     if (idx < currentActive) {
+//       circle.classList.add("on");
+//     } else {
+//       circle.classList.remove("on");
+//     }
+//   });
+
+//   const actives = document.querySelectorAll(".on");
+
+//   progress.style.width =
+//     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+// }
 
 //   if (currentActive === 1) {
 //     prev.disabled = true;
